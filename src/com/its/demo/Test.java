76b55fa92ecde869581bb5b78bdc09e3c0393769
KevinWu <sz.wuguoping@foxmail.com>
2016-01-12 17:43:33 +0800

    third commit.

--- src/com/its/demo/Test.java
+++ src/com/its/demo/Test.java
@@ -17,7 +17,7 @@ public class Test {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Hello World New!");
+		System.out.println("Hello World 20150112!");
 
 	}
 
